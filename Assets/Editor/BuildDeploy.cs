89c4b97a779755b7168feff3c9e1a51554cbffe5
chendahuang <chendahuang@910app.com>
2020-07-30 10:26:49 +0800

    first commit

---
+++ Assets/Editor/BuildDeploy.cs
@@ -0,0 +1,130 @@
+﻿//#define UNITY_IOS
+
+using UnityEngine;  
+using UnityEditor;  
+using System.Collections;  
+using System.Collections.Generic;  
+using System;  
+using System.IO; 
+using System.Text;
+#if UNITY_IOS
+using UnityEditor.iOS.Xcode; 
+#endif
+using Debug = UnityEngine.Debug;
+
+
+
+public class BuildDeploy : Editor
+{
+    private static string _bundleIdentifier = "com.xx.xxx";  // 打包的签名
+    private static string _version = "2.0";   // 版本号
+    private static string _buildVersion = "2.1";   // 版本号
+    public static string PUBLISH_PATH = Application.dataPath + "/../Publish/"; // 发布的资源目录，注意这个是apk的输出路径而不是ab包的输出路径
+    
+
+
+    // 编译程序
+    public static void Build() 
+    {
+        // EditorUserBuildSettings.connectProfiler = false;
+
+        // 打包成apk
+        var version = _version;
+		BuildTarget buildTarget = BuildTarget.iOS;  // 导出平台
+		string targetName = "Unity-iPhone";
+        BuildTargetGroup buildTargetGroup = BuildTargetGroup.iOS;
+
+
+        if (!Directory.Exists(PUBLISH_PATH))
+        {
+            Directory.CreateDirectory(PUBLISH_PATH);
+        }
+
+        // 删除之前的包
+        if (File.Exists(PUBLISH_PATH + targetName))
+        {
+            File.Delete(PUBLISH_PATH + targetName);
+        }
+
+		// PlayerSettings.bundleIdentifier = "com.square.naoligame3";
+        PlayerSettings.bundleVersion = version;
+        PlayerSettings.iOS.buildNumber = _buildVersion;
+
+        // 当前场景都打包进app里面
+        var scenes = new string[EditorBuildSettings.scenes.Length];
+        for (int i = 0; i < scenes.Length; ++i)
+        {
+            scenes[i] = EditorBuildSettings.scenes[i].path;
+        }
+
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        options.locationPathName = Path.GetFullPath(PUBLISH_PATH + targetName); // 注意5.6之后的版本，这个路径里面不能有Assets/这样的字符串，所以要转换为FullPath
+        options.scenes = scenes;
+        options.target = buildTarget;
+        options.targetGroup = buildTargetGroup;
+        options.options = BuildOptions.None; //uildOptions.CompressWithLz4;// | BuildOptions.AcceptExternalModificationsToPlayer;
+
+
+        // 开始构建
+		string msg = BuildPipeline.BuildPlayer(options);
+        if (!string.IsNullOrEmpty(msg))
+        {
+            Debug.LogError(msg);
+        }
+
+    }
+
+
+    // 解析命令行
+    public static void ParseCommandLine()
+    {
+        PlayerSettings.iOS.appleEnableAutomaticSigning = false;
+        // 设置当前的id
+        string[] args = Environment.GetCommandLineArgs();
+        if (args.Length <= 0) return;
+
+        for (int i = 0; i < args.Length - 1; ++i)
+        {
+            string arg = args[i];
+            if (arg == "-bundleId")
+            {
+                // 包唯一id
+                _bundleIdentifier = args[i + 1];
+            }
+            else if (arg == "-platform")
+            {
+                // 切换平台
+                string platform = args[i + 1];
+                if (platform == "android")
+                {
+                    if (EditorUserBuildSettings.activeBuildTarget != BuildTarget.Android)
+                    {
+                        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
+                    }
+                }
+                else if (platform == "ios")
+                {
+                    if (EditorUserBuildSettings.activeBuildTarget != BuildTarget.iOS)
+                    {
+                        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.iOS, BuildTarget.iOS);
+                    }
+                }
+            }
+            else if (arg == "-teamId")
+            {
+//#if UNITY_IOS
+                if (args[i + 1] != "")
+                {
+                    // TeamID ios打包使用
+                    PlayerSettings.iOS.appleEnableAutomaticSigning = true;
+                    PlayerSettings.iOS.appleDeveloperTeamID = args[i + 1];
+                }
+                
+//#endif
+            }
+        }
+
+    }
+
+
+}
---
+++ Jenkinsfile
@@ -0,0 +1,51 @@
+node{
+
+	def gitUser = 'yunmu_git'
+	// 签名 ios打包使用
+    def CODE_SIGN_IDENTITY = params.证书
+    def PROVISIONING_PROFILE = params.PROVISIONING_PROFILE
+    def platform = params.平台
+    def automatically = params.IsAutomatically
+    def teamID = params.TeamID
+    def bundleID = params.BundleID
+
+	// 默认取当前工作目录，当前工作目录在固态硬盘
+    def workspace = pwd()
+    // 如果有指定工作目录，则设置工作目录
+    if (params.工作目录 != '') {
+        workspace = params.工作目录 + '/' + env.JOB_NAME
+    }
+
+    stage('更新代码'){
+    	git([url: 'https://github.com/GeWenL/UnityJenkins.git', branch: params.分支, credentialsId: gitUser])
+    }
+
+    stage('制作整包') {
+
+        def unitybin = '/Applications/Unity/Unity.app/Contents/MacOS/Unity'
+        def build = 'OnAutoBuild'
+        if (automatically)
+        {
+            sh "${unitybin} -batchMode -quit -projectPath ${workspace} -executeMethod EditorToolMenu.${build} -bundleId ${bundleID} -platform ${platform} -teamId ${teamID}"
+        }
+        else
+        {
+            sh "${unitybin} -batchMode -quit -projectPath ${workspace} -executeMethod EditorToolMenu.${build} -bundleId ${bundleID} -platform ${platform}"
+        }
+ 		
+    }
+
+    stage('ios打包') {
+        if (params.平台 == 'ios') {
+            // ios平台需要调用xcode打出ipa包
+            sh "xcodebuild clean -project Publish/Unity-iPhone/Unity-iPhone.xcodeproj -configuration Release -alltargets"
+            sh "xcodebuild archive -project Publish/Unity-iPhone/Unity-iPhone.xcodeproj -scheme Unity-iPhone -archivePath Publish/Unity-iPhone.xcarchive"
+            sh "xcodebuild -exportArchive -archivePath Publish/Unity-iPhone.xcarchive -exportPath Publish -exportOptionsPlist Build/CodeSign/ios/exportOptions.plist  CODE_SIGN_IDENTITY=${CODE_SIGN_IDENTITY} PROVISIONING_PROFILE=${PROVISIONING_PROFILE} DEVELOPMENT_TEAM=${teamID}"
+            sh "mv Publish/Unity-iPhone.ipa Publish/${bundleId}-${version}.ipa"
+        }
+    }
+
+    stage('上传') {
+
+    }
+}
